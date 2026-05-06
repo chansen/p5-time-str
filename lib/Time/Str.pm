6397f23a9ab4b41975cb22a96ef9584c77b7fa4f
Christian Hansen <chansen@cpan.org>
2026-05-06 07:03:51 +0200

    Bump Time::Str version to 0.07

--- lib/Time/Str.pm
+++ lib/Time/Str.pm
@@ -9,7 +9,7 @@ our %EXPORT_TAGS = ( all => \@EXPORT_OK );
 use Exporter qw[import];
 
 BEGIN {
-  our $VERSION        = '0.06';
+  our $VERSION        = '0.07';
   our $IMPLEMENTATION = 'PP';
 
   my @import;
