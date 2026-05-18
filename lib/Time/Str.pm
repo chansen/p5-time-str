7cf2fc0ef1941d7224c3067ee7e8c2e613df8245
Christian Hansen <chansen@cpan.org>
2026-05-18 22:19:44 +0200

    Bump Time::Str modules to v0.70

--- lib/Time/Str.pm
+++ lib/Time/Str.pm
@@ -3,10 +3,11 @@ use strict;
 use warnings;
 use v5.10.1;
 
+use Carp     qw[];
 use Exporter qw[import];
 
 BEGIN {
-  our $VERSION     = '0.60';
+  our $VERSION     = '0.70';
   our @EXPORT_OK   = qw[ str2date
                          str2time
                          time2str ];
