940cbeeffec747cff78f2c2ccce580c24cef69d3
Christian Hansen <chansen@cpan.org>
2026-05-23 20:45:53 +0200

    Append benchmark results to bench/tz_vs.pl

--- bench/tz_vs.pl
+++ bench/tz_vs.pl
@@ -84,3 +84,25 @@ say "-" x 80;
 
 Benchmark::cmpthese(-10, \%Local_Bench);
 
+__END__
+
+Perl v5.42 (M1 Pro)
+
+Time: 2024-12-24T12:30:45Z TZID: Europe/Stockholm
+
+UTC offset - UTC
+--------------------------------------------------------------------------------
+                      Rate DateTime::Lite   DateTime DateTime::Tzfile Time::TZif
+DateTime::Lite     76853/s             --       -88%             -90%       -99%
+DateTime          657902/s           756%         --             -13%       -88%
+DateTime::Tzfile  754143/s           881%        15%               --       -86%
+Time::TZif       5278211/s          6768%       702%             600%         --
+
+UTC offset - Local
+--------------------------------------------------------------------------------
+                      Rate DateTime::Lite DateTime::Tzfile   DateTime Time::TZif
+DateTime::Lite     75487/s             --             -88%       -88%       -96%
+DateTime::Tzfile  605619/s           702%               --        -7%       -68%
+DateTime          653492/s           766%               8%         --       -66%
+Time::TZif       1903376/s          2421%             214%       191%         --
+
