de01583ebde99efaa3350b5a15ebbd3b93bbae3e
Christian Hansen <chansen@cpan.org>
2026-05-24 12:55:13 +0200

    Add find_tzdb_directory to Time::Str::Util
    Locate the system's IANA Time Zone Database directory by probing
    $TZDIR and well-known paths across Linux, macOS, FreeBSD, Solaris,
    and Cygwin. Returns undef if no valid directory is found.
    Reorganize POD into Binary Search and Time Zone Database sections.

--- lib/Time/Str/Util.pm
+++ lib/Time/Str/Util.pm
@@ -3,6 +3,7 @@ use strict;
 use warnings;
 use v5.10.1;
 
+use Carp     qw[croak];
 use Exporter qw[import];
 
 BEGIN {
@@ -16,6 +17,36 @@ BEGIN {
   require Time::Str;
   unless (Time::Str::IMPLEMENTATION() eq 'XS') {
     require Time::Str::PP; Time::Str::PP::Util->import(@EXPORT_OK);
+  }
+  
+  push @EXPORT_OK, qw[ find_tzdb_directory ];
+}
+
+{
+  # Directories to probe, in order of preference.
+  # Covers Linux, macOS, FreeBSD, Solaris, and Cygwin.
+  my @TZDB_CANDIDATES = qw(
+    /usr/share/zoneinfo
+    /usr/lib/zoneinfo
+    /usr/share/lib/zoneinfo
+    /etc/zoneinfo
+    /usr/share/zoneinfo.default
+  );
+
+  sub find_tzdb_directory {
+    @_ == 0 or croak q/Usage: find_tzdb_directory()/;
+
+    return $ENV{TZDIR} if defined $ENV{TZDIR} && -d $ENV{TZDIR};
+
+    foreach my $dir (@TZDB_CANDIDATES) {
+      return $dir if -d $dir && -f "$dir/UTC";
+    }
+
+    # macOS: /var/db/timezone/zoneinfo is a symlink to the active version
+    my $macos = '/var/db/timezone/zoneinfo';
+    return $macos if -d $macos && -f "$macos/UTC";
+
+    return undef;
   }
 }
 
