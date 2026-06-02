d8cd5820ecbeba67c2f27aa19b83813b461543d2
Christian Hansen <chansen@cpan.org>
2026-06-02 20:22:14 +0200

    Bump Time::Str module versions to 0.89

--- lib/Time/Str/Util.pm
+++ lib/Time/Str/Util.pm
@@ -7,7 +7,7 @@ use Carp     qw[croak];
 use Exporter qw[import];
 
 BEGIN {
-  our $VERSION     = '0.88';
+  our $VERSION     = '0.89';
   our @EXPORT_OK   = qw[ binary_search
                          lower_bound
                          range_bounds
